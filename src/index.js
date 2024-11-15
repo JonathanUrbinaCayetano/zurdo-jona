c9bc74591179d48492e31c8c387389bc79157d51
jonathan <jhon7122298039@gmail.com>
2024-11-15 12:42:33 -0600

    Primer commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ListaDocentes from './Trabajadores/Docente';
-
+import Alumnos from './Trabajadores/Alumnos'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ListaDocentes/>
+    <Alumnos/>
   </React.StrictMode>
 );
 
